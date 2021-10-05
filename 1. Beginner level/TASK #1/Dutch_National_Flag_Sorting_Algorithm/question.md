0879f9269c1bdd190b9f499c49b88d1c01b708eb
Akhil <gvakhilca1@gmail.com>
2021-10-05 10:53:26 +0530

    Added DNFsort and updated data.json

---
+++ 1. Beginner level/TASK #1/Dutch_National_Flag_Sorting_Algorithm/question.md
@@ -0,0 +1,1 @@
+Dutch National Flag Sorting algorithm
---
+++ 1. Beginner level/TASK #1/Dutch_National_Flag_Sorting_Algorithm/solution.java
@@ -0,0 +1,45 @@
+public class solution {
+    static void DNFsort(int[] a, int arr_size) {
+        int lo = 0;
+        int hi = arr_size - 1;
+        int mid = 0, temp = 0;
+        while (mid <= hi) {
+            switch (a[mid]) {
+                case 0: {
+                    temp = a[lo];
+                    a[lo] = a[mid];
+                    a[mid] = temp;
+                    lo++;
+                    mid++;
+                    break;
+                }
+                case 1:
+                    mid++;
+                    break;
+                case 2: {
+                    temp = a[mid];
+                    a[mid] = a[hi];
+                    a[hi] = temp;
+                    hi--;
+                    break;
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] arr, int arr_size) {
+        int i;
+        for (i = 0; i < arr_size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    /*Driver function to check for above functions*/
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+        int arr_size = arr.length;
+        DNFsort(arr, arr_size);
+
+        printArray(arr, arr_size);
+    }
+}
