a1e22378b5d53eb4501963a8a213baf3d66dfa98
Dinoguy12349 <yahia.nawashi@gmail.com>
2024-09-30 18:08:26 +0300

    Create swap.cpp

---
+++ Beginner Level 📁/swap/swap.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b=6;
+    cout <<a<<" "<<b<<endl;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout <<a<<" "<<b<<endl;
+    
+}
