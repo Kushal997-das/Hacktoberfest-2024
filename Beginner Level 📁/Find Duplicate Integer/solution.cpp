bd043029055361120dd7e1a79883278f5f7cf89b
Vimal M <109758195+Vimall03@users.noreply.github.com>
2024-10-02 13:37:23 +0530

    Add "Find Duplicate Integer" problem and solution

---
+++ Beginner Level 📁/Find Duplicate Integer/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(nums[i]) != map.end()) {
+                return nums[i];
+            }
+            map[nums[i]]++;
+        }
+        return -1; 
+};
+}
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 3, 4, 2, 2}; 
+    int duplicate = solution.findDuplicate(nums);
+    
+    cout << "The duplicate number is: " << duplicate << endl; 
+    return 0;
+}
