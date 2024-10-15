593d9fe2273f274f830568df8a7fb8f11af0bfb5
Shalu Patidar <136828396+shalupatidar22@users.noreply.github.com>
2024-10-15 21:31:18 +0530

    solved the issue "search insert position" in cpp

---
+++ Beginner Level 📁/Search Insert Position/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int searchInsertPosition(vector<int> arr, int target) {
+    int n = arr.size();
+    int l = 0;
+    int h = n - 1;
+    int ans = n;
+    while (l <= h) {
+        int mid = (l + h) / 2;
+        if (arr[mid] <= target) {
+            l = mid + 1;
+        } else {
+            ans = mid;  
+            h = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> a = {1, 3, 4, 5, 6};
+    int t = 2;
+    cout << searchInsertPosition(a, t);
+}
